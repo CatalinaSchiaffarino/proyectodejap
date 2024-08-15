27fcac3d49accb9c270eeb464414d2ff458ba0d1
catalinaschiaffarino <catalinaschiaffarinomartinez@gmail.com>
2024-08-15 19:30:14 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,20 @@
-document.addEventListener("DOMContentLoaded", function(){
+if (!localStorage.getItem("usuario")) {
+    window.location.href = "login.html";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
-        window.location = "products.html"
+        window.location.href = "products.html";
     });
+
     document.getElementById("juguetes").addEventListener("click", function() {
         localStorage.setItem("catID", 102);
-        window.location = "products.html"
+        window.location.href = "products.html";
     });
+
     document.getElementById("muebles").addEventListener("click", function() {
         localStorage.setItem("catID", 103);
-        window.location = "products.html"
+        window.location.href = "products.html";
     });
 });
