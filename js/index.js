c261140cd83b4d5f8fa4202073d414ca16ca9f0c
catherine-rocha <cathyrocha18@hotmail.com>
2024-08-19 18:29:25 -0300

    Guardar y Cerrar Sesión

--- js/index.js
+++ js/index.js
@@ -1,14 +1,28 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("autos").addEventListener("click", function () {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
     });
-    document.getElementById("juguetes").addEventListener("click", function() {
+    document.getElementById("juguetes").addEventListener("click", function () {
         localStorage.setItem("catID", 102);
         window.location = "products.html"
     });
-    document.getElementById("muebles").addEventListener("click", function() {
+    document.getElementById("muebles").addEventListener("click", function () {
         localStorage.setItem("catID", 103);
         window.location = "products.html"
     });
+    //validar que el localStorage este dentro de nuestro navegador(Saber si inició sesión)
+    let ObjUsuario = JSON.parse(localStorage.getItem("usuario"));
+    if (!localStorage.getItem("usuario") && !localStorage.getItem("contraseña")) { //si no lo puedo hacer hago un location.href
+        location.href = "login.html";
+     }
+    if (localStorage.getItem("usuario") && localStorage.getItem("contraseña")){
+        document.getElementById("user").innerHTML = "Cliente: " + ObjUsuario;
+    }
+    // borrar localStorage(Cerrar Sesión)
+    document.getElementById("cerrar").addEventListener("click", function () {
+        localStorage.removeItem("usuario");
+        localStorage.removeItem("contraseña");
+     });
+
 });
